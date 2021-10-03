dede12224a7283362077352c84f0897f3c0487e4
sandeshpath <sandeshpathak38@gmail.com>
2021-10-03 17:13:51 +0530

    DOM API selection Methods

--- assests/Css/js/app.js
+++ assests/Css/js/app.js
@@ -1,59 +1,15 @@
-// Arthmetic operators
+var heading = document.getElementById('heading')
+//heading.innerHTML ="Hi from i am java"
 
-var a = 20
-var b = 10
-   
+//console.log(heading)
 
+var para = document.getElementsByClassName('para')
+//console.log(para.length)
+//console.log(para[0])// selecting one element 
+//console.log(para[1])
 
-console.log('a+b=',a+b)
-console.log('a-b=',-b)
-console.log('a/b=',a/b)
-console.log('a*b=',a*b)
-console.log('a**b=',a**b) //power operator
-console.log('a%b=',a%b)
+var tags = document.getElementsByTagName('a')
+console.log(tags)
 
-//Assignment Operators
-
-b = 13
-console.log('b ',b)
-b+= 10
-console.log('b+=',b)
-b++
-console.log("b++",b)
-
-//Comparison Operators
-
-
-var d = 10
-var e =2
-var f='10'
-
-console.log(d>e)
-
-console.log(d<e)
-console.log(d==e)
-
-console.log(d>=e)
-
-console.log(d<=e)
-
-
-console.log(d==f)
-console.log(d===f)
-console.warn( d + f)
-
-// Logical operator
-// and operator
-console.log( "true && true=",true && true)
-console.log( "false && true=",false && true)
-console.log( "true && false=",true && false)
-console.log( "false && false=",false && false)
-//or operator
-console.log( "true && true=",true ||true)
-console.log( "false && true=",false || true)
-console.log( "true && false=",true || false)
-console.log( "false && false=",false || false)
-
-
-
-
+var query = document.querySelector('#heading')
+console.log(query)
