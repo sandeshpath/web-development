765dcd0526ac7f13001b243b4d3eb3269d6db2b5
sandeshpath <sandeshpathak38@gmail.com>
2021-10-03 15:21:34 +0530

    primitive data types

---
+++ assests/Css/js/app.js
@@ -0,0 +1,13 @@
+// Data types
+ //String
+console.log("Hello world")
+console.warn( typeof"hellow warn")
+console.log(typeof"a")
+//Number
+console.log(typeof 1)
+console.log(typeof -121213)
+
+//boolean
+
+console.log(typeof true)
+console.log(typeof false)
