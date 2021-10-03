48706fbc77bd7c6e8424d56e5b436a4ee4395678
sandeshpath <sandeshpathak38@gmail.com>
2021-10-03 15:28:48 +0530

    null vs undefined

--- assests/Css/js/app.js
+++ assests/Css/js/app.js
@@ -1,13 +1,3 @@
-// Data types
- //String
-console.log("Hello world")
-console.warn( typeof"hellow warn")
-console.log(typeof"a")
-//Number
-console.log(typeof 1)
-console.log(typeof -121213)
+var age=null
 
-//boolean
-
-console.log(typeof true)
-console.log(typeof false)
+console.log(typeof age)
