1ab3094f8219965235775b8569b46c55592a5ad1
sandeshpath <sandeshpathak38@gmail.com>
2021-10-03 15:38:39 +0530

    constant vale and array

--- assests/Css/js/app.js
+++ assests/Css/js/app.js
@@ -1,3 +1,21 @@
-var age=null
+// Array
+//array are not generic 
+// it is move by top to bottom
 
-console.log(typeof age)
+var arr = [ 1,2,3,4,'sandesh']
+
+arr.push(6)
+
+console.log(arr)
+console.log(typeof arr)
+console.log( arr.length)
+
+// change the vale of variable
+var age = 10
+age = 20
+console.log(age)
+
+//you can not change the value then use constant
+const age =15
+age = 12
+console.log(age)
